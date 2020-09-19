30fdcd9ebb8bf11160aa2016a96b4167bc5cda9c
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2020-09-19 18:22:56 +0530

    Euler Toitent For 1 to N

---
+++ edu/math/concepts/EulerToitentFor_1_to_N.java
@@ -0,0 +1,39 @@
+package edu.math.concepts;
+
+import java.util.Scanner;
+
+public class EulerToitentFor_1_to_N {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] phi = getAllPhiFromOneToN(n);
+
+        for (int i = 1; i < phi.length; i++) {
+            System.out.println(i + " - " + phi[i] + " ");
+        }
+        sc.close();
+    }
+
+    static int[] getAllPhiFromOneToN(int n) {
+        int[] phi = new int[n + 1];
+
+        phi[0] = 0;
+        phi[1] = 1;
+
+        for (int i = 2; i < phi.length; i++) {
+            phi[i] = i;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (phi[i] == i) {
+                for (int j = i; j <= n; j += i) {
+                    phi[j] = phi[j] - (phi[j] / j);
+                }
+            }
+        }
+
+        return phi;
+    }
+}
