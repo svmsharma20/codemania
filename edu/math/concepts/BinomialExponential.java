54372efff7c806f3c6b124867ac2ff18d6d59adb
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2020-10-09 13:15:47 +0530

    BinomialExponential

---
+++ edu/math/concepts/BinomialExponential.java
@@ -0,0 +1,28 @@
+package edu.math.concepts;
+
+import java.util.Scanner;
+
+public class BinomialExponential {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(pow(a, n));
+        sc.close();
+    }
+
+    static public int pow(int a, int n) {
+        int _result = 1;
+        int _pow = a;
+        int i = 1;
+        while (n > 0) {
+            if ((n & i) > 0) {
+                _result *= _pow;
+            }
+            _pow = _pow * _pow;
+
+            n = n >> 1;
+        }
+        return _result;
+    }
+}
