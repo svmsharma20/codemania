db21321b84acef9ebee034a56a6b247aeca336c9
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2020-10-11 09:28:15 +0530

    Refractored name

--- edu/math/concepts/BinaryExponential.java
+++ edu/math/concepts/BinaryExponential.java
@@ -2,7 +2,7 @@ package edu.math.concepts;
 
 import java.util.Scanner;
 
-public class BinomialExponential {
+public class BinaryExponential {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
