1872b94faac13cb4b3bc155a05d4879b3b288e89
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-03-22 10:51:47 +0530

    1. Length of Last Word 2. Reverse the String

---
+++ edu/interviewBit/string/LengthOfLastWord.java
@@ -0,0 +1,24 @@
+package edu.interviewBit.string;
+
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(final String A) {
+        if(A==null){
+            return 0;
+        }
+        int lenOfString = A.length()-1;
+        int lenOfLastWord=0;
+
+        while(lenOfString>=0 && A.charAt(lenOfString)==' '){
+            lenOfString--;
+        }
+
+        while(lenOfString>=0 && A.charAt(lenOfString)!=' '){
+            lenOfLastWord++;
+            lenOfString--;
+        }
+
+        return lenOfLastWord;
+    }
+
+}
