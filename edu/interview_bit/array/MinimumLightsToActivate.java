bab7aac1f4544f45db17ba6be475fb73b870e6ff
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-05-22 13:58:16 +0530

    Minimum Lights To Activate with much simpler code Repeat And Missing Number Array

--- edu/interview_bit/array/MinimumLightsToActivate.java
+++ edu/interview_bit/array/MinimumLightsToActivate.java
@@ -3,7 +3,68 @@ package edu.interview_bit.array;
 import java.util.ArrayList;
 
 public class MinimumLightsToActivate {
-    
+
+    // https://www.youtube.com/watch?v=CQ0Tlu3o7Yg
+    // There is bug in the video(in the inner loop while condition; j<A.size() should not be present there),
+    // but this algo has resolve that as well, by moving it to if condition.
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(0);
+        list.add(1);
+        list.add(1);
+        list.add(0);
+        list.add(0);
+        list.add(1);
+
+        System.out.println(new MinimumLightsToActivate().solveWithMoreSimpleCode(list, 3));
+
+    }
+
+    public int solveWithMoreSimpleCode(ArrayList<Integer> A, int B) {
+        int i=0;
+        int count=0;
+
+        while (i<A.size()){
+            int j = i+(B-1);
+            boolean isBulbFound = false;
+
+            while (j>=(i-B+1) && j>=0){
+                /*
+                * The first condition tells that j is currently out of range as compare to size fo the array list
+                * But, it might come after some iteration.
+                * This will happen in below case:
+                *
+                *   A: [0, 1, 0, 1, 1, 0, 0, 1]
+                *   B: 3  // power of the bulb
+                *
+                * In this case, we will enlighten bulb at 1st and 4th index.
+                * After that, bulb at 7th index is still off and also not getting light.
+                * In this scenario j=i+(B-1) => j=7+(3-1) => j=9
+                *
+                * So we have to wait until j becomes less than A.size()
+                * */
+
+                if(j<A.size() && A.get(j)==1){
+                    count++;
+                    i = j+B;
+                    isBulbFound = true;
+                    break;
+                }
+                j--;
+            }
+
+            if(!isBulbFound){
+                return -1;
+            }
+        }
+
+        return count;
+    }
+
+
     public int solve(ArrayList<Integer> A, int B) {
         
         if(A==null || A.size()==0){
--- edu/interview_bit/array/RepeatAndMissingNumberArray.java
+++ edu/interview_bit/array/RepeatAndMissingNumberArray.java
@@ -1,8 +1,24 @@
 package edu.interview_bit.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RepeatAndMissingNumberArray {
 
     // https://www.youtube.com/watch?v=5nMGY4VUoRY
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>();
+        A.add(3);
+        A.add(1);
+        A.add(2);
+        A.add(5);
+        A.add(3);
+
+        System.out.println(new RepeatAndMissingNumberArray().repeatedNumbersUsingXOR(A));
+    }
+
     public int[] repeatedNumber(final int[] A) {
         
         long sum = 0, sumSquares = 0;
@@ -12,6 +28,62 @@ public class RepeatAndMissingNumberArray {
         }
         long AminusB = sum, AplusB = sumSquares / sum;
         return new int[] {(int)((AplusB+AminusB)/2), (int)((AplusB-AminusB)/2)};
+    }
+
+    public List<Integer> repeatedNumbersUsingXOR(final List<Integer> A){
+        ArrayList<Integer> a = new ArrayList<Integer>();
+
+        int ss=0;
+        for (int i = 1; i <= A.size(); i++) {
+            ss ^= i;
+        }
+
+        for(int i : A) {
+            ss ^= i;
+        }
+
+        int c = 0;
+        for(int i = 0; i < 64; i++) {
+            if(((1 << i) & ss) > 0) {
+                c = i;
+                break;
+            }
+        }
+
+        int f = 0, s = 0;
+
+        for(int i = 1; i <= A.size(); i++) {
+            if(((1 << c) & i) > 0) {
+                f ^= i;
+            } else {
+                s ^= i;
+            }
+        }
+
+        for(int i : A) {
+            if(((1 << c) & i) > 0) {
+                f ^= i;
+            } else {
+                s ^= i;
+            }
+        }
+
+        for(int i : A) {
+            if(i == f) {
+                a.add(f);
+                a.add(s);
+                break;
+            }
+
+            if(i == s) {
+                a.add(s);
+                a.add(f);
+                break;
+            }
+        }
+
+        return a;
+
     }
     
 }
