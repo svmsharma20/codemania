f4b71938b7fcf608f06510c9896f39681ba55159
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-05-28 10:36:53 +0530

    Added method to generate Linked list with value within range

--- edu/concepts/linkedlist/LinkListUtility.java
+++ edu/concepts/linkedlist/LinkListUtility.java
@@ -4,16 +4,20 @@ import java.util.Random;
 
 public class LinkListUtility {
     
-    static ListNode createListWithRandomNumbers(int size){
-        Random rand = new Random();
-        ListNode newNode = null;
-        for (int i = 0; i < size; i++) {
-            newNode = new ListNode(rand.nextInt(100)-50, newNode);
-        }
-        return newNode;
+    public static ListNode createListWithRandomNumbers(int size){
+        return createListWithRandomNumbers(size, -50, 51);
     }
-    
-    static void printList(ListNode node){
+
+    public static ListNode createListWithRandomNumbers(int size, int min, int max){
+      Random rand = new Random();
+      ListNode newNode = null;
+      for (int i = 0; i < size; i++) {
+        newNode = new ListNode(rand.nextInt(max - min) + min, newNode);
+      }
+      return newNode;
+    }
+
+    public static void printList(ListNode node){
         System.out.println();
          while(node!=null){
              System.out.print(node.val+" ");
