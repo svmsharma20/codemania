062febf8f4541ed1bc9b45dc242c6bf88d398252
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-06-10 16:27:08 +0530

    Arrange Buildings

---
+++ edu/pepcoding/dynamic_programming/ArrangeBuildings.java
@@ -0,0 +1,30 @@
+package edu.pepcoding.dynamic_programming;
+
+import java.util.Scanner;
+
+public class ArrangeBuildings {
+
+  public static void main(String[] args) {
+    long n = new Scanner(System.in).nextLong();
+
+    System.out.println(getTotalArrangement(n));
+  }
+
+  private static long getTotalArrangement(long n) {
+    if(n==0){
+      return 0;
+    }
+    long prevB = 1;
+    long prevS = 1;
+
+    for (int i = 2; i <= n; i++) {
+      long currentB = prevS;
+      long currentS = prevB+prevS;
+      prevB = currentB;
+      prevS = currentS;
+    }
+
+    long totalArrangementsPerSide = prevB + prevS;
+    return totalArrangementsPerSide * totalArrangementsPerSide;
+  }
+}
