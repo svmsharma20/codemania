76fa0f241c38ebff47cd316b4f3a1213cca8f581
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-06-12 08:42:42 +0530

    Maximum Sum Non-Adjacent Elements

---
+++ edu/pepcoding/dynamic_programming/MaximumSumNonAdjacentElements.java
@@ -0,0 +1,36 @@
+package edu.pepcoding.dynamic_programming;
+
+import java.util.Scanner;
+
+public class MaximumSumNonAdjacentElements {
+
+  // https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/maximum-sum-non-adjacent-elements-official/ojquestion
+  // https://youtu.be/VT4bZV24QNo?list=TLGG-g-kz4wW-7wxMjA2MjAyMQ
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.println(getMaxSumNonAdjacent(arr));
+  }
+
+  private static int getMaxSumNonAdjacent(int[] arr) {
+    final int INCLUDE = 0;
+    final int EXCLUDE = 1;
+
+    int[][] dp = new int[2][arr.length];
+    dp[INCLUDE][0] = arr[0];
+    dp[EXCLUDE][0] = 0;
+
+    for (int i = 1; i < arr.length; i++) {
+      dp[INCLUDE][i] = arr[i] + dp[EXCLUDE][i-1];
+      dp[EXCLUDE][i] = Math.max(dp[INCLUDE][i-1], dp[EXCLUDE][i-1]);
+    }
+
+    return Math.max(dp[INCLUDE][arr.length-1], dp[EXCLUDE][arr.length-1]);
+  }
+}
