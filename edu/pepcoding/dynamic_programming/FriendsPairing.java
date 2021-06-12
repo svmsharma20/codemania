8545787500062ba8099eb9aeaf6189830821d40e
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-06-12 19:47:18 +0530

    Friends Pairing

---
+++ edu/pepcoding/dynamic_programming/FriendsPairing.java
@@ -0,0 +1,23 @@
+package edu.pepcoding.dynamic_programming;
+
+import java.util.Scanner;
+
+public class FriendsPairing {
+
+  // https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/friends-pairing-official/ojquestion
+  // https://youtu.be/SHDu0Ufjyk8?list=TLGGzkehcy5Ob1ExMjA2MjAyMQ
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int[] dp = new int[n+1];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i-1] + (i-1)*dp[i-2];
+    }
+
+    System.out.println(dp[n]);
+  }
+}
