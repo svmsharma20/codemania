bee0b4a7317092735e428ad19a5dccee1e825973
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-06-19 12:29:10 +0530

    Maximum Sum Increasing Subsequence

---
+++ edu/pepcoding/dynamic_programming_2/MaximumSumIncreasingSubsequence.java
@@ -0,0 +1,36 @@
+package edu.pepcoding.dynamic_programming_2;
+
+import java.util.Scanner;
+
+public class MaximumSumIncreasingSubsequence {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.println(getMaxSumOfIS(arr));
+  }
+
+  private static int getMaxSumOfIS(int[] arr) {
+    int[] dp = new int[arr.length];
+    int maxSum = arr[0];
+
+    for (int i = 0; i < arr.length; i++) {
+      int localMax = 0;
+      for (int j = 0; j < i; j++) {
+        if(arr[j]<arr[i]){
+          localMax = Math.max(localMax, dp[j]);
+        }
+      }
+      dp[i] = localMax + arr[i];
+      maxSum = Math.max(maxSum, dp[i]);
+    }
+
+    return maxSum;
+  }
+}
