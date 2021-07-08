567774de85675d2fc6aae441e805378179be34c4
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-07-08 21:34:00 +0530

    Levelorder Linewise

---
+++ edu/pepcoding/tree/LevelorderLinewise.java
@@ -0,0 +1,31 @@
+package edu.pepcoding.tree;
+
+import java.util.ArrayDeque;
+
+// https://www.pepcoding.com/resources/online-java-foundation/generic-tree/levelorder-linewise-generic-tree-official/ojquestion
+public class LevelorderLinewise {
+
+  public static void levelOrderLinewise(Node node){
+    Node invNode = new Node();
+    invNode.data = -1;
+    ArrayDeque<Node> queue = new ArrayDeque<>();
+    queue.add(node);
+    queue.add(invNode);
+    String str = "";
+    while(!queue.isEmpty()){
+      Node peek = queue.removeFirst();
+      if(peek.data == -1){
+        System.out.println(str);
+        str = "";
+        if(!queue.isEmpty()){
+          queue.add(invNode);
+        }
+      }else{
+        str += peek.data + " ";
+        for(Node child : peek.children){
+          queue.add(child);
+        }
+      }
+    }
+  }
+}
