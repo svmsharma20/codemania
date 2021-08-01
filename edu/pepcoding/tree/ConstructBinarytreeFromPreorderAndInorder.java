b09fb0336a3ec1f3812341763b3c410e15dcd4d1
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-08-01 18:36:41 +0530

    Construct Binarytree From Preorder And Inorder Traversal

---
+++ edu/pepcoding/tree/ConstructBinarytreeFromPreorderAndInorder.java
@@ -0,0 +1,45 @@
+package edu.pepcoding.tree;
+
+import java.util.HashMap;
+
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/trees/construct-binarytree-from-preorder-and-inorder-traversal/ojquestion
+public class ConstructBinarytreeFromPreorderAndInorder {
+
+  static int preIndex = 0;
+  static HashMap<Integer, Integer> map = new HashMap<>();
+  public static class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static TreeNode buildTree(int[] preorder, int[] inorder) {
+    for (int i = 0; i < inorder.length; i++) {
+      map.put(inorder[i], i);
+    }
+    return buildMyTree(preorder, inorder, 0, preorder.length-1);
+  }
+
+  public static TreeNode buildMyTree(int[] preorder, int[] inorder, int inStart, int inEnd) {
+    if(inStart > inEnd){
+      return null;
+    }
+
+    TreeNode node = new TreeNode(preorder[preIndex++]);
+
+    if(inStart == inEnd){
+      return node;
+    }
+
+    int idx = map.get(node.val);
+
+    node.left = buildMyTree(preorder, inorder, inStart, idx-1);
+    node.right = buildMyTree(preorder, inorder, idx+1, inEnd);
+
+    return node;
+  }
+}
