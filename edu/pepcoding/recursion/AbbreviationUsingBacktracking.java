c55bc29154a01d898830233030016e6f9440719c
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-08-07 14:13:18 +0530

    Abbreviation Using Backtracking

---
+++ edu/pepcoding/recursion/AbbreviationUsingBacktracking.java
@@ -0,0 +1,35 @@
+package edu.pepcoding.recursion;
+
+import java.util.Scanner;
+
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/abbreviation-suing-backtracking-official/ojquestion
+public class AbbreviationUsingBacktracking {
+
+  public static void solution(String str, String asf, int count, int pos) {
+    if (pos == str.length()) {
+      if (count == 0) {
+        System.out.println(asf);
+      } else {
+        System.out.println(asf + count);
+      }
+      return;
+    }
+
+    char ch = str.charAt(pos);
+    if (count > 0) {
+      solution(str, asf + count + ch, 0, pos + 1);
+    } else {
+      solution(str, asf + ch, 0, pos + 1);
+    }
+
+    solution(str, asf, count + 1, pos + 1);
+
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.nextLine();
+    solution(str, "", 0, 0);
+  }
+
+}
