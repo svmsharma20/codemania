94cab7ebf13f5d066e7713f23c9f43e3a84dfcdd
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-09-18 20:28:54 +0530

    Count set bits

--- edu/tests/CountTotalSetBits.java
+++ edu/tests/CountTotalSetBits.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class CountTotalSetBits {
 
+  //  https://www.youtube.com/watch?v=_o7QBzM33J0
   public static void main(String[] args) {
+
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     System.out.println(getCountOfSetBitsWithoutMathLib(n));
