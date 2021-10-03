c4f82f23be8ef8c0f903654fd18552c727ddb07d
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-10-03 08:19:28 +0530

    Build Array From Permutation

---
+++ edu/leetcode/array/BuildArrayFromPermutation.java
@@ -0,0 +1,21 @@
+package edu.leetcode.array;
+
+/*
+ * https://leetcode.com/problems/build-array-from-permutation/
+ * */
+public class BuildArrayFromPermutation {
+
+  public int[] buildArray(int[] nums) {
+    int len = nums.length;
+    for (int i = 0; i < nums.length; i++) {
+      int r = nums[i];
+      int q = nums[r] % len;
+      nums[i] = q * len + r;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      nums[i] = nums[i] / len;
+    }
+    return nums;
+  }
+}
