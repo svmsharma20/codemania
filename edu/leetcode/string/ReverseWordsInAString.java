28bd5606feca5cd42e3260e018ff5313874f2701
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-10-07 07:33:49 +0530

    Reverse Words in a String

---
+++ edu/leetcode/string/ReverseWordsInAString.java
@@ -0,0 +1,32 @@
+package edu.leetcode.string;
+
+public class ReverseWordsInAString {
+
+  public String reverseWords(String s) {
+
+    int flagCount = 0;
+
+    StringBuilder sb = new StringBuilder();
+    StringBuilder res = new StringBuilder();
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+      char ch = s.charAt(i);
+      if (ch != ' ') {
+        sb.insert(0, ch);
+        flagCount = 0;
+      } else if (sb.length() > 0) {
+        sb.append(' ');
+        res.append(sb);
+        sb = new StringBuilder();
+      }
+    }
+
+    if (sb.length() > 0) {
+      res.append(sb);
+    }
+    if (res.charAt(res.length() - 1) == ' ') {
+      res.deleteCharAt(res.length() - 1);
+    }
+    return res.toString();
+  }
+}
