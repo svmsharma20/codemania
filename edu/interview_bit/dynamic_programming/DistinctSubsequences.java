ec7cffa66959becfbb2427f8fb419c7d667097b4
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-10-28 10:41:21 +0530

    Distinct Subsequences

--- edu/interview_bit/dynamic_programming/DistinctSubsequences.java
+++ edu/interview_bit/dynamic_programming/DistinctSubsequences.java
@@ -32,4 +32,28 @@ public class DistinctSubsequences {
 
         return dp[i][j];
     }
+
+    public int numDistinctIter(String A, String B) {
+        int[][] dp = new int[B.length() + 1][A.length() + 1];
+        dp[B.length()][A.length()] = 1;
+        for (int i = dp.length - 1; i >= 0; i--) {
+            for (int j = dp[0].length - 2; j >= 0; j--) {
+                if (i == dp.length - 1) {
+                    dp[i][j] = 1;
+                } else if (A.charAt(j) == B.charAt(i)) {
+                    dp[i][j] = dp[i][j + 1] + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = dp[i][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        String A = "aaaababbababbaabbaaababaaabbbaaabbb";
+        String B = "bbababa";
+
+        System.out.println(new DistinctSubsequences().numDistinctIter(A, B));
+    }
 }
